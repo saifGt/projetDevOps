7f3a6dc55a0e7afa755a0b14cb83ba72c6f5391b
saifGt <saif.meddeb.52@gmail.com>
2025-04-01 04:29:06 -1200

    Votre message de commit

--- src/main/java/tn/esprit/spring/services/CourseServicesImpl.java
+++ src/main/java/tn/esprit/spring/services/CourseServicesImpl.java
@@ -1,37 +1,51 @@
 package tn.esprit.spring.services;
 
 import lombok.AllArgsConstructor;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.stereotype.Service;
 import tn.esprit.spring.entities.Course;
-import tn.esprit.spring.entities.TypeCourse;
 import tn.esprit.spring.repositories.ICourseRepository;
 
 import java.util.List;
+
 @AllArgsConstructor
 @Service
-public class CourseServicesImpl implements  ICourseServices{
+public class CourseServicesImpl implements ICourseServices {
 
-    private ICourseRepository courseRepository;
+    private static final Logger logger = LogManager.getLogger(CourseServicesImpl.class);
+    private final ICourseRepository courseRepository;
 
     @Override
     public List<Course> retrieveAllCourses() {
-        return courseRepository.findAll();
+        logger.info("Récupération de tous les cours...");
+        List<Course> courses = courseRepository.findAll();
+        logger.debug("Nombre de cours récupérés : {}", courses.size());
+        return courses;
     }
 
     @Override
     public Course addCourse(Course course) {
-        return courseRepository.save(course);
+        logger.info("Ajout d'un nouveau cours : {}", course);
+        Course savedCourse = courseRepository.save(course);
+        logger.debug("Cours ajouté avec ID : {}", savedCourse.getNumCourse());
+        return savedCourse;
     }
 
     @Override
     public Course updateCourse(Course course) {
-        return courseRepository.save(course);
+        logger.info("Mise à jour du cours avec ID : {}", course.getNumCourse());
+        Course updatedCourse = courseRepository.save(course);
+        logger.debug("Cours mis à jour avec succès : {}", updatedCourse);
+        return updatedCourse;
     }
 
     @Override
     public Course retrieveCourse(Long numCourse) {
-        return courseRepository.findById(numCourse).orElse(null);
+        logger.info("Récupération du cours avec ID : {}", numCourse);
+        return courseRepository.findById(numCourse).orElseGet(() -> {
+            logger.error("Cours avec ID {} non trouvé", numCourse);
+            return null;
+        });
     }
-
-
 }
